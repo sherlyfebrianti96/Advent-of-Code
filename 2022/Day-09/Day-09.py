979b11c1958282bd9fe108f6c51e7f476d08de20
Sherly Febrianti <sherlyfebrianti96@yahoo.co.id>
2022-12-10 03:47:42 +0100

    refactor Day 09

--- 2022/Day-09/Day-09.py
+++ 2022/Day-09/Day-09.py
@@ -13,12 +13,12 @@ def coordinateStr(coordinate: dict):
 
 
 # Function for checking the Stepping process
-def steppingProcess(direction: str, numberOfSteps: int, inputHead: dict, inputTail: dict):
-    head = inputHead.copy()
-    tail = inputTail.copy()
-    numberOfStepsTail = 0
-    steppedByTail = []
-    prevHead = {"x": head['x'], "y": head['y']}
+def steppingProcess(direction: str, numberOfSteps: int, inputKnots: list, inputSteppedByTail: list):
+    knots = inputKnots.copy()
+    numberOfStepsTail = [0 for knot in knots]
+    steppedByTail = inputSteppedByTail.copy()
+
+    prevHead = [{"x": knot['x'], "y": knot['y']} for knot in knots]
 
     print('\n==================\n')
     print('direction : ', direction)
@@ -31,50 +31,54 @@ def steppingProcess(direction: str, numberOfSteps: int, inputHead: dict, inputTa
     # If the direction is RIGHT, it means the X is +1
 
     for step in range(0, numberOfSteps):
-        match direction:
-            case 'U':
-                print('\n--------------\n')
-                print("UP :")
-                head["y"] -= 1
-            case 'D':
-                print('\n--------------\n')
-                print("DOWN :")
-                head["y"] += 1
-            case 'L':
-                print('\n--------------\n')
-                print("LEFT :")
-                head["x"] -= 1
-            case 'R':
-                print('\n--------------\n')
-                print("RIGHT :")
-                head["x"] += 1
-            case other:
-                # Do nothing
+        for index, knot in enumerate(knots):
+
+            if (index == 0):
                 pass
+            else:
+                head = knots[index-1]
+                tail = knots[index]
+                match direction:
+                    case 'U':
+                        print('\n--------------\n')
+                        print("UP :")
+                        head["y"] -= 1
+                    case 'D':
+                        print('\n--------------\n')
+                        print("DOWN :")
+                        head["y"] += 1
+                    case 'L':
+                        print('\n--------------\n')
+                        print("LEFT :")
+                        head["x"] -= 1
+                    case 'R':
+                        print('\n--------------\n')
+                        print("RIGHT :")
+                        head["x"] += 1
+                    case other:
+                        # Do nothing
+                        pass
 
-        # Check if the Head and Tail position is side-by-side
-        # If it is not side-by-side, we need to make the tail take over the last position of the head
-        distanceX = abs(head["x"] - tail["x"])
-        distanceY = abs(head["y"] - tail["y"])
-        if (distanceX > 1 or distanceY > 1):
-            tail['x'] = prevHead['x']
-            tail['y'] = prevHead['y']
-            numberOfStepsTail += 1
-            steppedByTail.append(coordinateStr(tail))
+                # Check if the Head and Tail position is side-by-side
+                # If it is not side-by-side, we need to make the tail take over the last position of the head
+                distanceX = abs(head["x"] - tail["x"])
+                distanceY = abs(head["y"] - tail["y"])
+                if (distanceX > 1 or distanceY > 1):
+                    tail['x'] = prevHead[index-1]['x']
+                    tail['y'] = prevHead[index-1]['y']
+                    numberOfStepsTail[index] += 1
+                    steppedByTail[index].append(coordinateStr(tail))
 
-        print('head : ', head)
-        print('tail : ', tail)
-        prevHead['x'] = head['x']
-        prevHead['y'] = head['y']
+                print('head : ', head)
+                print('tail : ', tail)
+                prevHead[index-1]['x'] = head['x']
+                prevHead[index-1]['y'] = head['y']
 
     result = {
         'numberOfStepsTail': numberOfStepsTail,
         'steppedByTail': steppedByTail,
-        'head': head,
-        'tail': tail
+        'knots': knots
     }
-    print('\n--------------\n')
-    print('result : ', result)
 
     return result
 
@@ -92,17 +96,10 @@ for command in commands:
     direction = data[0]
     numberOfSteps = int(data[1])
 
-    for index, knot in enumerate(knots):
-        if (index == 0):
-            pass
-        else:
-            result = steppingProcess(
-                direction, numberOfSteps, knots[index - 1], knot)
-
-            # Update the Knots data
-            knots[index-1] = result['head']
-            knots[index] = result['tail']
-            knotsStepped[index] += result['steppedByTail']
+    result = steppingProcess(direction, numberOfSteps, knots, knotsStepped)
+    knots = result['knots']
+    knotsStepped = result['steppedByTail']
+    print('result : ', result)
 
 
 lastKnotSteppedUnique = set(knotsStepped[-1])
